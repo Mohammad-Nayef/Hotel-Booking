88e593766338658c1a2e216c69db28a2f5310624
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-17 16:08:19 +0200

    Prepared the project for the migration

--- HotelBooking.API/Program.cs
+++ HotelBooking.API/Program.cs
@@ -2,17 +2,13 @@ using HotelBooking.Db.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<HotelsBookingDbContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
