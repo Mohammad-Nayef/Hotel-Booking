4e0a3cb9097dea99a4d3a8c48348c79a631fd35f
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-19 00:04:58 +0200

    Created user repository

---
+++ HotelBooking.Db/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using HotelBooking.Domain.Abstractions.Repositories;
+using HotelBooking.Domain.Exceptions;
+using HotelBooking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBooking.Db.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private readonly HotelsBookingDbContext _dbContext;
+
+        public UserRepository(HotelsBookingDbContext dbContext)
+            : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void ThrowExceptionIfIdOrUsernameExists(User userToFind)
+        {
+            bool usernameExists = false;
+            
+            _dbContext.Users.ForEachAsync(user =>
+            {
+                if (user.Id == userToFind.Id)
+                    throw new IdDuplicationException(user.Id);
+
+                usernameExists = user.Username == userToFind.Username;
+            });
+
+            if (usernameExists) 
+                throw new UsernameDuplicationException(userToFind.Username);
+        }
+    }
+}
---
+++ HotelBooking.Domain/Abstractions/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using HotelBooking.Domain.Models;
+using HotelBooking.Domain.Exceptions;
+
+namespace HotelBooking.Domain.Abstractions.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        /// <summary>
+        /// Searches for the existence of the Id, if not found, it searches for the
+        /// existence of the username.
+        /// </summary>
+        /// <param name="user">The user to search for its Id and username</param>
+        /// <exception cref="IdDuplicationException"></exception>
+        /// <exception cref="UsernameDuplicationException"></exception>
+        void ThrowExceptionIfIdOrUsernameExists(User user);
+    }
+}
