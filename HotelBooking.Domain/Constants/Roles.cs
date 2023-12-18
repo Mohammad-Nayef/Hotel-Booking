efecec4d9a8034da19e03ac0ac111cbc45c7d9f9
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-18 14:30:00 +0200

    Created roles constants

---
+++ HotelBooking.Domain/Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelBooking.Domain.Constants
+{
+    public class Roles
+    {
+        public const string Admin = "Admin";
+        public const string RegularUser = "RegularUser";
+    }
+}
