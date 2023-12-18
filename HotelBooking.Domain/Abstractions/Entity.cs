137276526d51b54ca6d6c2253867f754ebdb5285
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-18 13:42:46 +0200

    Created entity service and repository abstractions

---
+++ HotelBooking.Domain/Abstractions/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace HotelBooking.Domain.Abstractions
+{
+    public abstract class Entity
+    {
+        Guid Id { get; set; }
+    }
+}
---
+++ HotelBooking.Domain/Abstractions/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBooking.Domain.Abstractions
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> GetAllAsync(int itemsTake, int itemsSkip);
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task<bool> AddAsync(TEntity entity);
+        Task<bool> UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
