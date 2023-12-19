c721842bfad1069ac824015c4b77837289793481
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-19 09:51:46 +0200

    Created custom add method for users containing username validation

---
+++ HotelBooking.Application/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using HotelBooking.Domain.Abstractions.Repositories;
+using HotelBooking.Domain.Abstractions.Services;
+using HotelBooking.Domain.Models;
+using Microsoft.AspNet.Identity;
+
+namespace HotelBooking.Application.Services
+{
+    public class UserService : EntityService<User>, IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public UserService(IUserRepository userRepository, IPasswordHasher passwordHasher)
+            : base(userRepository)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            ArgumentNullException.ThrowIfNull(user, nameof(user));
+
+            _userRepository.ThrowExceptionIfIdOrUsernameExists(user);
+
+            user.Password = _passwordHasher.HashPassword(user.Password);
+
+            await _userRepository.AddAsync(user);
+        }
+    }
+}
---
+++ HotelBooking.Application/Validations/PaginationValidation.cs
@@ -0,0 +1,22 @@
+﻿namespace HotelBooking.Application.Validations
+{
+    public class PaginationValidation
+    {
+        public static bool Validate(int pageNumber, int pageSize)
+        {
+            var negativePageNumber = pageNumber <= 0;
+            var negativePageSize = pageSize <= 0;
+
+            if (negativePageNumber && negativePageSize)
+                throw new Exception($"'{pageNumber}' and '{pageSize}' must be greater than 0.");
+
+            if (negativePageNumber)
+                throw new Exception($"'{pageNumber}' must be greater than 0.");
+
+            if (negativePageSize)
+                throw new Exception($"'{pageSize}' must be greater than 0.");
+
+            return true;
+        }
+    }
+}
