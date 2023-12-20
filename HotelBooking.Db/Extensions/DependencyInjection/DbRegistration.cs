7408d952661eef9b6562f70c78747b25cba1d4d2
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-20 13:11:15 +0200

    Created IoC services collection extension

---
+++ HotelBooking.Db/Extensions/DependencyInjection/DbRegistration.cs
@@ -0,0 +1,17 @@
+﻿using HotelBooking.Db.Repositories;
+using HotelBooking.Domain.Abstractions.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelBooking.Db.Extensions.DependencyInjection
+{
+    public static class DbRegistration
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services)
+        {
+            services.AddDbContext<HotelsBookingDbContext>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
