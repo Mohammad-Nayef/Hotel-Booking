b55e69dadf753cbe8d7e8f70b4040ec1e9a6dc24
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-20 13:14:30 +0200

    Created an extension for IoC services collection

---
+++ HotelBooking.Application/Extensions/DependencyInjection/DomainServicesRegistration.cs
@@ -0,0 +1,16 @@
+﻿using HotelBooking.Application.Services;
+using HotelBooking.Domain.Abstractions.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelBooking.Application.Extensions.DependencyInjection
+{
+    public static class DomainServicesRegistration
+    {
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
