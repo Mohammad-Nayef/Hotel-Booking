91cbcae187cc3d84b1a6aba7b00b26b148d060b3
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-22 00:00:53 +0200

    Project files modifications

--- HotelBooking.API/Program.cs
+++ HotelBooking.API/Program.cs
@@ -1,11 +1,24 @@
-using HotelBooking.Db.DataAccess;
+using System.Reflection;
+using FluentValidation;
+using HotelBooking.Application.Extensions.DependencyInjection;
+using HotelBooking.Db.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSwaggerGen(setup =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+    setup.IncludeXmlComments(xmlCommentsFullPath);
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<HotelsBookingDbContext>();
+builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+builder.Services.AddDatabase();
+builder.Services.AddDomainServices();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
