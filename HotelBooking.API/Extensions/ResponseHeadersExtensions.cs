1e03fb2020c6e4911563dcb3f9d624dbb6dce95f
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-22 00:01:34 +0200

    Added API extensions

---
+++ HotelBooking.API/Extensions/ResponseHeadersExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using HotelBooking.Api.Models;
+using HotelBooking.Domain.Models;
+
+namespace HotelBooking.Api.Extensions
+{
+    internal static class ResponseHeadersExtensions
+    {
+        public const string PaginationKeyName = "X-Pagination";
+
+        public static void AddPaginationMetadata(
+            this IHeaderDictionary headers,
+            int totalItems,
+            PaginationDTO pagination)
+        {
+            var paginationMetadata = new PaginationMetadataDTO(totalItems, pagination);
+            var jsonPaginationMetadata = JsonSerializer.Serialize(paginationMetadata);
+
+            headers.Add(PaginationKeyName, jsonPaginationMetadata);
+        }
+    }
+}
---
+++ HotelBooking.API/Extensions/ValidationExceptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using HotelBooking.Api.Models;
+
+namespace HotelBooking.Api.Extensions
+{
+    internal static class ValidationExceptionExtensions
+    {
+        public static IEnumerable<ValidationResultDTO> GetErrorsForClient(
+            this ValidationException validationException)
+        {
+            return validationException.Errors.Select(error => new ValidationResultDTO
+            {
+                ErrorMessage = error.ErrorMessage,
+                PropertyName = error.PropertyName,
+                AttemptedValue = error.AttemptedValue.ToString()
+            });
+        }
+    }
+}
