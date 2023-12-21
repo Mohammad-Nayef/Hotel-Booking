7f0772ef5f2fdb4c2cc8cb0c69d3dc1dafdd5683
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-22 00:02:59 +0200

    Created room controller

---
+++ HotelBooking.API/Controllers/RoomController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using FluentValidation;
+using HotelBooking.Api.Extensions;
+using HotelBooking.Api.Models;
+using HotelBooking.Domain.Abstractions.Services;
+using HotelBooking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/rooms")]
+    public class RoomController : Controller
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+
+        public RoomController(IRoomService roomService, IMapper mapper)
+        {
+            _roomService = roomService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Create and store a new room.
+        /// </summary>
+        /// <param name="newRoom">Properties of the new room.</param>
+        /// <response code="201">Returns the room with a new Id and its URI in response headers.</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(RoomCreationDTO), StatusCodes.Status201Created)]
+        public async Task<IActionResult> PostAsync(RoomCreationDTO newRoom)
+        {
+            Guid newId;
+
+            try
+            {
+                newId = await _roomService.AddAsync(_mapper.Map<RoomDTO>(newRoom));
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.GetErrorsForClient());
+            }
+
+            var createdRoom = _mapper.Map<RoomForCreationResponseDTO>(newRoom);
+            createdRoom.Id = newId;
+
+            return Created($"api/rooms/{newId}", createdRoom);
+        }
+
+        /// <summary>
+        /// Delete a room with a specific Id.
+        /// </summary>
+        /// <param name="id">The Id of the room to delete.</param>
+        /// <response code="404">The room with the given Id doesn't exist.</response>
+        /// <response code="204">The room is deleted successfully.</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            try
+            {
+                await _roomService.DeleteAsync(id);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+            return NoContent();
+        }
+
+    }
+}
