996e7ccd349a11381c61b2a5e70df3bd92a8884f
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-22 07:10:09 +0200

    Created a database view for getting hotels for admins

---
+++ HotelBooking.Db/Migrations/20231222044556_c.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBooking.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class c : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoleDTOUserDTO");
+
+            migrationBuilder.CreateTable(
+                name: "RoleTableUserTable",
+                columns: table => new
+                {
+                    RolesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleTableUserTable", x => new { x.RolesId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_RoleTableUserTable_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleTableUserTable_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleTableUserTable_UsersId",
+                table: "RoleTableUserTable",
+                column: "UsersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoleTableUserTable");
+
+            migrationBuilder.CreateTable(
+                name: "RoleDTOUserDTO",
+                columns: table => new
+                {
+                    RolesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleDTOUserDTO", x => new { x.RolesId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_RoleDTOUserDTO_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleDTOUserDTO_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleDTOUserDTO_UsersId",
+                table: "RoleDTOUserDTO",
+                column: "UsersId");
+        }
+    }
+}
---
+++ HotelBooking.Db/Migrations/20231222044620_ss.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBooking.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class ss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                CREATE VIEW vw_HotelsForAdmin AS
+                SELECT Id, Name, StarRating, OwnerName, CreationDate, ModificationDate,
+                    (SELECT COUNT(*) FROM Rooms
+                    WHERE Rooms.HotelId = Hotels.Id)
+                    As NumberOfRooms
+                FROM Hotels
+                """);
+    }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW vw_HotelsForAdmin");
+        }
+    }
+}
